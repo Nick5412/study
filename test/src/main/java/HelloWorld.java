94c40e7d6dfb7af0f94f3570520cbfb67b16861b
Nick <100649695+nick5412@users.noreply.github.com>
2023-07-13 22:57:39 +0800

    HelloWorld
    Signed-off-by: Nick <1374175412@qq.com>

---
+++ test/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @Description TODO
+ * @Classname HelloWorld
+ * @Date 7/13 013 22:54
+ * @Created by Nick
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
